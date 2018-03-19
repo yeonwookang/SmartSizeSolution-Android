4892fdab3db54a77dfc5f9db6394abd2768dd642
DESKTOP-8BDI4BL\jekan <jekang0123@gmail.com>
2018-03-19 12:26:33 +0900

    스플래시 버그 고침

--- FYP/app/src/main/java/com/example/jekan/fyp_test/activity/SplashActivity.java
+++ FYP/app/src/main/java/com/example/jekan/fyp_test/activity/SplashActivity.java
@@ -8,7 +8,10 @@ import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.jekan.fyp_test.R;
 import com.felipecsl.gifimageview.library.GifImageView;
 
@@ -18,39 +21,40 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class SplashActivity extends AppCompatActivity {
-    private GifImageView splashImage;
+
+    ImageView splashImg;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        splashImage = (GifImageView) findViewById(R.id.img_splash_background);
-        try {
-            InputStream inputStream = getAssets().open("sss_splash.gif");
-            byte[] bytes = IOUtils.toByteArray(inputStream);
-            splashImage.setBytes(bytes);
-            splashImage.startAnimation();
-        } catch (IOException ex) {
 
-        }
+//        RelativeLayout layout = (RelativeLayout) findViewById(R.id.splashRelative);
+//        layout.setBackgroundDrawable(new BitmapDrawable(getResources(), BitmapFactory.decodeResource(getResources(), R.drawable.background_splash)));
 
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                SplashActivity.this.startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                SplashActivity.this.finish();
-            }
-        }, 2500);
+        splashImg = (ImageView)findViewById(R.id.img_splash_background);
+        Glide.with(splashImg.getContext()).load(R.drawable.sss_splash).asGif().diskCacheStrategy(DiskCacheStrategy.NONE).into(splashImg);
+        Handler handler = new Handler();
+        handler.postDelayed(new SplashHandler(), 3500);
     }
 
-    /*@Override
+    /* 로딩 화면 스플래시 핸들러 */
+    private class SplashHandler implements Runnable {
+        public void run() {
+            Intent i = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(i);
+            SplashActivity.this.finish();
+        }
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
-        recycleView(findViewById(R.id.activity_splash_screen));
+        recycleView(findViewById(R.id.img_splash_background));
         System.gc();
     }
 
+    /* 메모리 관리를 위한 리사이클 함수*/
     private void recycleView(View view) {
         if (view != null) {
             Drawable bg = view.getBackground();
@@ -59,56 +63,6 @@ public class SplashActivity extends AppCompatActivity {
                 ((BitmapDrawable) bg).getBitmap().recycle();
                 view.setBackgroundDrawable(null);
             }
-        }*/
-
-    /*
-
-    private class SplashHandler implements Runnable {
-        public void run() {
-            Intent i = new Intent(SplashActivity.this, MainActivity.class);
-            startActivity(i);
-            SplashActivity.this.finish();
         }
     }
-
-    ImageView imageView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        imageView = (ImageView)findViewById(R.id.img_splash_background) ;
-        Handler handler = new Handler();
-        handler.postDelayed(new SplashHandler(), 1000);
-    }
-
-
-    private class SplashHandler implements Runnable {
-        public void run() {
-            Intent i = new Intent(SplashActivity.this, MainActivity.class);
-            startActivity(i);
-            SplashActivity.this.finish();
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        //recycleView(findViewById(R.id.img_splash_background));
-         recycleView(imageView);
-        System.gc();
-    }
-
-    private void recycleView(ImageView view) {
-        if (view != null) {
-            GlideDrawableImageViewTarget splashImage = new GlideDrawableImageViewTarget(view);
-            Glide.with(this).load(R.drawable.img_splash).into(splashImage);
-            Drawable bg = view.getBackground();
-            if (bg != null) {
-                bg.setCallback(null);
-                ((BitmapDrawable) bg).getBitmap().recycle();
-                view.setBackgroundDrawable(null);
-            }
-        }
-    }*/
-    }
+}
